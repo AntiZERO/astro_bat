5c19141bacfb45a4e174f3a6f69defb9c0d55ddf
AntiZERO <gray2884@gmail.com>
2025-01-18 16:24:29 -0600

    more fixes

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ class AstroBat(discord.Client):
         self.nasa = NASANews(NASA_API_KEY)
         self.astronomy_channel = None
         self.guild = None
+        self.check_space_news = self.check_space_news_loop
         
         # Future feature ideas:
         # - Track ISS passes over Austin
@@ -24,28 +25,34 @@ class AstroBat(discord.Client):
     async def on_ready(self):
         print(f"{BOT_EMOJI} {BOT_NAME} is ready to explore the cosmos! {STAR_EMOJI}")
 
-        # Check if the bot is connected to any guilds
-        if len(self.guilds) > 0:
-            # Get the guild (server) object
-            self.guild = self.guilds[0]
+        try:
+            # Check if the bot is connected to any guilds
+            if len(self.guilds) > 0:
+                # Get the guild (server) object
+                self.guild = self.guilds[0]
 
-            # Get the astronomy channel from the guild
-            self.astronomy_channel = self.guild.get_channel(ASTRONOMY_CHANNEL_ID)
-            if self.astronomy_channel is None:
-                print(f"{BOT_EMOJI} Lost in space - can't find the 'astronomy' channel!")
-        else:
-            print(f"{BOT_EMOJI} Lost in space - the bot is not connected to any servers!")
+                # Get the astronomy channel from the guild
+                self.astronomy_channel = self.guild.get_channel(ASTRONOMY_CHANNEL_ID)
+                if self.astronomy_channel is None:
+                    print(f"{BOT_EMOJI} Lost in space - can't find the 'astronomy' channel!")
+            else:
+                print(f"{BOT_EMOJI} Lost in space - the bot is not connected to any servers!")
+        except Exception as e:
+            print(f"{BOT_EMOJI} Encountered an error: {e}")
 
     @tasks.loop(seconds=UPDATE_INTERVAL)
     async def check_space_news(self):
-        if self.astronomy_channel is None:
-            print(f"{BOT_EMOJI} Lost in space - can't find the 'astronomy' channel!")
-            return
+        try:
+            if self.astronomy_channel is None:
+                print(f"{BOT_EMOJI} Lost in space - can't find the 'astronomy' channel!")
+                return
 
-        nasa_post = await self.nasa.get_astronomy_pic()
-        if nasa_post:
-            embed = make_post_pretty(nasa_post)
-            await self.astronomy_channel.send(embed=embed)
+            nasa_post = await self.nasa.get_astronomy_pic()
+            if nasa_post:
+                embed = make_post_pretty(nasa_post)
+                await self.astronomy_channel.send(embed=embed)
+        except Exception as e:
+            print(f"{BOT_EMOJI} Encountered an error: {e}")
 
 def main():
     print(f"{BOT_EMOJI} {BOT_NAME} preparing for launch...")
